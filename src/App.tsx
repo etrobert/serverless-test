4da027b61e6204fe7f457cd8bcf870857061519d
erobert <etiennerobert33@gmail.com>
2020-11-28 12:25:03 +0100

    display message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState<string>('Loading');
+  useEffect(() => {
+    const loadMessage = async () => {
+      const response = await fetch('.netlify/functions/todos-read-all');
+      if (!response.ok) {
+        setMessage('Error');
+        return;
+      }
+      const message = await response.text();
+      setMessage(message);
+    };
+    loadMessage();
+  }, []);
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
